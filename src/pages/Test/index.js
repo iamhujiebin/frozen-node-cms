74af86bdaae4f0edd7be7e1ef94ab291354fa6f7
iamhujiebin <458249864@qq.com>
2023-06-29 11:54:40 +0800

    svga播放

--- src/pages/Test/index.js
+++ src/pages/Test/index.js
@@ -1,16 +1,23 @@
 import {useEffect, useRef} from "react";
 import Vap from 'video-animation-player'
+import SVGA from 'svgaplayerweb'
 import config from './demo.json'
 import demo from './demo.mp4'
 import config2 from './vapc.json'
 import demo2 from './video.mp4'
 import man from "@/assets/man.png"
 import woman from "@/assets/woman.png"
+import car from "./car.svga"
+import cp from "./cp.svga"
 
 
 function Test() {
-    let vapPlayer = useRef(null);
-    let vapPlayer2 = useRef(null);
+    let vapPlayer = useRef(null)
+    let vapPlayer2 = useRef(null)
+    let svgaPlayer = useRef(null)
+    let svgaParser = useRef(null)
+    let svgaPlayer2 = useRef(null)
+    let svgaParser2 = useRef(null)
 
     useEffect(() => {
         vapPlayer.current = new Vap().play(Object.assign({}, {
@@ -59,9 +66,29 @@ function Test() {
             // 融合信息（图片/文字）,同素材生成工具生成的配置文件中的srcTag所对应，比如[imgUser] => imgUser
             name: "皇马人",
         }))
-    });
+    }, [])
+    useEffect(() => {
+        svgaParser.current = new SVGA.Parser()
+        svgaPlayer.current = new SVGA.Player('#demoCanvas')
+        // svgaParser.current.load("https://oss.iludo.live/nextvideo/gifts/4cf8b7cec0df4fb3a80d05aa2281049a.svga", function (videoItem) {
+        svgaParser.current.load(car, function (videoItem) {
+            svgaPlayer.current.setVideoItem(videoItem)
+            svgaPlayer.current.startAnimation()
+        })
+    }, [])
+    useEffect(() => {
+        svgaParser2.current = new SVGA.Parser()
+        svgaPlayer2.current = new SVGA.Player('#demoCanvas2')
+        svgaParser2.current.load(cp, function (videoItem) {
+            svgaPlayer2.current.setVideoItem(videoItem)
+            svgaPlayer2.current.startAnimation()
+            svgaPlayer2.current.setImage(man, 'sender_avatar');
+        })
+    }, [])
     return (
         <>
+            <div id='demoCanvas2' style={{width: 100, height: 100}}></div>
+            <div id='demoCanvas' style={{width: 100, height: 100}}></div>
             <div ref={vapPlayer}/>
             <div ref={vapPlayer2}/>
         </>
