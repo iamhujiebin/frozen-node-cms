932eeb994565ab88b40f65a26d0eeb433e7b5a43
iamhujiebin <458249864@qq.com>
2023-07-14 10:23:15 +0800

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,8 +1,31 @@
-const array = [1, 2, 3, 4, 5];
-const number = 3;
+const apiid = '154701613091163251';
+const appkey = '058A3D7F167744A1A380CDE4B235F521';
+const token = 'biZRK+8y8XKeNnwgp3s2Qwo22b+IPtqkO2ZJb7t+7gaxrL036Pti11yJxeX6KYPC+KJHFDlQO4QQ1j3saCnqcg==';
+console.log(token); // encrypted token
 
-if (array.includes(number)) {
-    console.log(`${number} is in the array.`);
-} else {
-    console.log(`${number} is not in the array.`);
-}
+var axios = require("axios");
+var data = JSON.stringify({
+    "runModel": "10",
+    "prompt": "小雨中，两对情侣，互相撒娇。风格：清新",
+    "wh": "16:9",
+    "callbackUrl": "https://test.apiv1.faceline.live/midJourney"
+});
+
+var config = {
+    method: "post",
+    url: "https://www.api-clouds.cn/apiclouds/v1/image/generate",
+    headers: {
+        "Content-Type": "application/json",
+        "x-apiid": apiid,
+        "x-token": token,
+    },
+    data: data
+};
+axios(config).then(function (response) {
+    console.log(JSON.stringify(response.data));
+})
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
