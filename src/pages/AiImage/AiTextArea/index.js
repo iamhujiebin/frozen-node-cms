2560e77a8cd83fcb73bad303c522bb0d6c9f004c
iamhujiebin <458249864@qq.com>
2023-07-22 23:59:25 +0800

    Update index.js

--- src/pages/AiImage/AiTextArea/index.js
+++ src/pages/AiImage/AiTextArea/index.js
@@ -1,22 +1,25 @@
-import {Button, Input} from "antd";
+import {Button, Input, Space} from "antd";
 
 const {TextArea} = Input;
 
 const AiTextArea = ({prompt, setPrompt, placeholder, submit}) => {
     return (
         <>
-            <TextArea
-                size={"middle"}
-                showCount
-                maxLength={1000}
-                style={{
-                    height: 320,
-                }}
-                placeholder={placeholder}
-                value={prompt}
-                onChange={(e) => setPrompt(e.target.value)}
-            />
-            <Button type={"primary"} onClick={submit}>生成图片</Button>
+            <Space direction={"vertical"}>
+                <TextArea
+                    size={"middle"}
+                    showCount
+                    maxLength={1000}
+                    style={{
+                        height: 320,
+                        width: 280,
+                    }}
+                    placeholder={placeholder}
+                    value={prompt}
+                    onChange={(e) => setPrompt(e.target.value)}
+                />
+                <Button type={"primary"} onClick={submit}>生成图片</Button>
+            </Space>
         </>
     );
 };
